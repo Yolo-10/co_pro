77b570155027530666eb049b16325c09b5709aa4
Yolo-He <2684921675@qq.com>
2022-07-07 21:37:25 +0800

    updat list

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -9,7 +9,7 @@ const listPage=()=> {
   const {
     title,navList,
     list06_1,list07_1,list06_2,list07_2,list06_3,list07_3,
-    list07_4,list07_5,img04,sc4_title,img01
+    list07_4,list07_5,img04,sc4_title,img01,sc4_nearBox,sc4_inBox,
   } = list; 
 
   const handleLiClick = (index)=>{
@@ -26,18 +26,21 @@ const listPage=()=> {
       </div>
 
       <div className='m_bd'>
-            <section style={{"display":activeLi == 0? "block":"none"}}>
+        <section style={{"display":activeLi == 0? "block":"none"}}>
           <List07 data={list07_1}/>
           <List06 data={list06_1}/>
         </section>
+
         <section style={{"display":activeLi == 1? "block":"none"}}>
           <List07 data={list07_2}/>
           <List06 data={list06_2}/>
         </section>
+
         <section style={{"display":activeLi == 2? "block":"none"}}>
           <List07 data={list07_3}/>
           <List06 data={list06_3}/>
         </section>
+
         <section style={{"display":activeLi == 3? "block":"none"}} className="sc4">
           <List07 data={list07_4}/>
           <Title title={sc4_title} line="true"/>
@@ -45,11 +48,12 @@ const listPage=()=> {
           <img src="https://ginza-brc.jp/img/capsule02.jpg" alt="" />
           <div className='capsule_box'>
             <Img01 data={img01}/>
-            <p>ここでは書ききれないほどの効果が酸素にはあります！<br/>点滴の施術後に入っていただくことによって、投与された点滴が高気圧によって全身を巡り、酸素が身体への吸収を手助けしてくれるため通常時の約2～10倍の効果アップが期待されています。</p>
+            <p>{sc4_inBox}</p>
           </div>
-          <p>※スムーズにご案内できるよう予約を調整しておりますが、やむを得ずお待たせしてしまう可能性がございます。あらかじめご了承下さい。</p>
+          <p>{sc4_nearBox}</p>
           <List07 data={list07_5}/>
         </section>
+
       </div>
    
       <Cta data={cta}></Cta>
